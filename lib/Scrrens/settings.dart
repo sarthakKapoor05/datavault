c46add5b4dcafade42e7122519e5b44875ca43c0
sarthakKapoor05 <sarthakkapoor9151@gmail.com>
2025-04-07 12:18:19 +0530

    changes in settinge screen

--- lib/Scrrens/settings.dart
+++ lib/Scrrens/settings.dart
@@ -8,7 +8,7 @@ class Settings extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Setting Screen",
+          "Setting Screen make your chanegs here",
           style: TextStyle(
             fontSize: 25,
             fontWeight: FontWeight.bold,
