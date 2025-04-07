0ec4c4713c2b3978527f4a5dab1f67aac1400336
sarthakKapoor05 <sarthakkapoor9151@gmail.com>
2025-04-07 21:42:28 +0530

    changes

--- lib/Scrrens/home.dart
+++ lib/Scrrens/home.dart
@@ -1,5 +1,6 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-import 'package:datavault/Scrrens/files.dart';
+import 'package:datavault/Scrrens/dashboard.dart';
+// import 'package:datavault/Scrrens/files.dart';
 import 'package:datavault/Scrrens/settings.dart';
 import 'package:datavault/Scrrens/share.dart';
 import 'package:flutter/material.dart';
@@ -13,7 +14,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   int index = 0;
-  final screen = [Files(), Share(), Settings()];
+  final screen = [Dashboard(), Share(), Settings()];
 
   @override
   Widget build(BuildContext context) {
